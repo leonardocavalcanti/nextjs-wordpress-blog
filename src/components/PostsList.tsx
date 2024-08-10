f41298539181db1192b52f20a58ea8db310fc21a
Leonardo Cavalcanti <leonardo.cavalcanti91@hotmail.com>
2024-08-10 17:51:41 -0300

    build fix

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -9,7 +9,7 @@ export default async function PostsList({ page, search }: { page: number, search
     return (
       <div className="flex flex-col h-full items-center justify-center mt-12">
         <p className="text-2xl">Something went wrong</p>
-        <p className="text-gray-400">Please check the address you're trying to access or come back later</p>
+        <p className="text-gray-400">Please check the address you&apos;re trying to access or come back later</p>
       </div>
     );
   }
@@ -18,7 +18,7 @@ export default async function PostsList({ page, search }: { page: number, search
     <div className="flex flex-col gap-8">
       {posts.data.length === 0 && (
         <div className="flex flex-col h-full items-center justify-center mt-12">
-          <p className="text-2xl">Sorry, we couldn't find any posts {search ? `with the term "${search}"` : "here"}</p>
+          <p className="text-2xl">Sorry, we couldn&apos;t find any posts {search ? `with the term "${search}"` : "here"}</p>
           <p className="text-gray-400">Try searching for something else or come back later</p>
         </div>
       )}
